ff03a575d9ddee05ed15e81faba4a0c68b954198
Devang <risingentropy20@gmail.com>
2021-09-22 14:56:16 +0530

    basicAPIsDone

--- api/urls.py
+++ api/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('api/shows', Shows.as_view(), name='Shows'),
     path('api/show/<int:showID>', TheShow.as_view(), name="TheShow"),
     path('api/episode/<int:episodeID>',TheEpisodeData.as_view(),name="TheEpisode"),
+    path('api/reply/<int:commentID>/<str:username>',ReplyAPI.as_view(),name="ReplyAPI"),
+    path('api/comment/<int:episodeID>/<str:username>', CommentAPI.as_view(), name="CommentAPI"),
+    path('api/comment/<int:id>', theComment.as_view(), name="theComment"),
+    path('api/reply/<int:id>', theReply.as_view(), name="theReply"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,4 @@
+from django.db.models import deletion
 from django.shortcuts import render
 from rest_framework.views import APIView
 from django.http import HttpResponse, JsonResponse
@@ -43,14 +44,12 @@ class TheShow(APIView):
         except:
             return Response({"status": "404 Not Found", "message": "Show does not exist."})
 
-# TheEpisodeData - episodeID
-# All the Comments
-# All the replies to those comments
+
 class TheEpisodeData(APIView):
     def get(self, request, episodeID):
         try:
             theEpisode = Episode.objects.filter(id=episodeID)[0]
-            theComments = Comment.objects.filter(id=episodeID)
+            theComments = Comment.objects.filter(episode=theEpisode)
             thatEpisode = {
                     "id":theEpisode.id,
                     "show":theEpisode.show.id,
@@ -58,14 +57,15 @@ class TheEpisodeData(APIView):
                     "episodeNumber":theEpisode.episodeNumber,
                 }
             requiredComments = []
+            print("Length is"+str(len(theComments)))
             for comment in theComments:
                 theComment = {
                         "id": comment.id,
                         "user": {
                             "username": comment.user.username,
                             "email": comment.user.email,
-                            "firstname": comment.user.first_name,
-                            "lastname": comment.user.last_name,
+                            "first_name": comment.user.first_name,
+                            "last_name": comment.user.last_name,
                         },
                         "text": comment.text,
                         "episode":comment.episode.id,
@@ -86,8 +86,100 @@ class TheEpisodeData(APIView):
                     allReplies.append(theReply)
                 theComment["replies"] = allReplies
                 requiredComments.append(theComment)
+                print("Mera yassssuuuuu")
             return Response({"episode":thatEpisode,"comments":requiredComments})
         except:
             return Response({"status": "404 Not Found", "message": "Episode does not exist."})
 
-#http://localhost:8000/api/episode/1
+
+class CommentAPI(APIView):
+    def post(self, request, episodeID, username):
+        
+        try:
+            thatEpisode = Episode.objects.filter(id=episodeID)[0]
+        except:
+            return Response({"status": "404 Not Found", "message": "Episode does not exist."})
+
+        try:
+            thatUser = User.objects.filter(username=username)[0]
+        except:
+            return Response({"status": "404 Not Found", "message": "Episode does not exist."})
+        
+        text = request.data['text']
+
+        try:
+            addComment = Comment(episode=thatEpisode, user=thatUser, text=text)
+            addComment.save()
+        except:
+            return Response({"status": "500 Internal Server Error", "message": "database went through an error."})
+        
+        resp = {
+                "id": addComment.id,
+                "username": addComment.user.username,
+                "text": addComment.text,
+                "episodeID": addComment.episode.id,
+                "status": "201 Created"
+            }
+        return Response(resp)
+
+
+class ReplyAPI(APIView):
+    def post(self,request,commentID,username):
+        try:
+            thatComment = Comment.objects.filter(id=commentID)[0]
+        except:
+            return Response({"status": "404 Not Found", "message": "Comment does not exist."})
+        try:
+            thatUser = User.objects.filter(username=username)[0]
+        except:
+            return Response({"status": "404 Not Found", "message": "Episode does not exist."})
+        text = request.data["text"]
+        try:
+            addReply = Reply(comment=thatComment,user=thatUser,text=text)
+            addReply.save()
+        except:
+            return Response({"status": "500 Internal Server Error", "message": "database went through an error."})
+        resp = {
+            "id": addReply.id,
+            "username": addReply.user.username,
+            "text": addReply.text,
+            "commentID": addReply.comment.id,
+            "status": "201 Created"
+        }
+        return Response(resp)
+
+
+class theComment(APIView):
+    def delete(self, request, id):
+        comment = Comment.objects.filter(id=id)
+        if comment:
+            comment.delete()
+            return Response({"status":"Comment deleted successfully"})
+        return Response({"status": "404 Not Found", "message": "Comment does not exist."})
+       
+    
+    def put(self, request, id):
+        try:
+            text = request.data["text"]
+            comment = Comment.objects.filter(id=id).update(text=text)
+            return Response({"status":"Comment Updated successfully"})
+        except: 
+            return Response({"status": "404 Not Found", "message": "Comment does not exist."})
+
+
+class theReply(APIView):
+    def delete(self, request, id):
+        #Boooooooooo
+        reply = Reply.objects.filter(id=id)
+        if reply:
+            reply.delete()
+            return Response({"status":"Reply deleted successfully"})
+        return Response({"status": "404 Not Found", "message": "Reply does not exist."})
+        
+    def put(self, request, id):
+        try:
+            text = request.data["text"]
+            reply = Reply.objects.filter(id=id).update(text=text)
+            return Response({"status":"Reply Updated successfully"})
+        except: 
+            return Response({"status": "404 Not Found", "message": "Reply does not exist."})
